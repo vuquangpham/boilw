ef4342570f04ad06ee5c272fc73caecc3b4e1ab5
vuquangpham <vuquangpham2909@gmail.com>
2023-05-28 23:27:47 +0700

    fix(webpack): remove redundant option

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
@@ -21,29 +20,6 @@ module.exports = {
                     },
                 ],
             },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                type: "asset/resource",
-                generator: {
-                    filename: "assets/images/[hash][ext]",
-                },
-            },
-            // Fonts and SVGs: Inline files
-            {
-                test: /\.(woff(2)?|eot|ttf|otf)$/,
-                type: 'asset/inline',
-                generator: {
-                    filename: "assets/fonts/[hash][ext]",
-                },
-            },
         ]
     },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../', 'public', 'index.html'),
-            filename: 'index.html',
-            inject: 'head'
-        }),
-    ],
 };
