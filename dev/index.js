67c94c762e3a392e254dfb4b8f47daa6bf14906b
vuquangpham <vuquangpham2909@gmail.com>
2023-05-29 21:36:25 +0700

    feat(dev): general style

--- dev/index.js
+++ dev/index.js
@@ -4,4 +4,13 @@ import './style.css';
 // library
 import '@/_index.js';
 
-// code
+// package info
+import packageInfo from '../package.json';
+
+// update project information
+const dataTitles = document.querySelectorAll('[data-title]');
+const dataDescriptions = document.querySelectorAll('[data-description]');
+
+// update information
+dataTitles.forEach(e => e.innerHTML = packageInfo.name);
+dataDescriptions.forEach(e => e.innerHTML = packageInfo.description);
