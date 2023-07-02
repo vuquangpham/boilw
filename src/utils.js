8739200e63a508f8c58b80f051c4942d42f2a7e9
vuquangpham <vuquangpham2909@gmail.com>
2023-07-02 09:58:12 +0700

    chore: remove redundant

--- src/utils.js
+++ src/utils.js
@@ -22,7 +22,7 @@ export function debounce(func, timeout = 200){
  */
 export function uid(prefix = ''){
     return prefix + Date.now().toString(36) + Math.random().toString(36).slice(2);
-};
+}
 
 
 /**
