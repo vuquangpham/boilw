540e79b67960556883471de23af7be33689502cd
vuquangpham <vuquangpham2909@gmail.com>
2023-10-31 00:52:57 +0700

    fix: add html loader

--- config/config.js
+++ config/config.js
@@ -83,6 +83,10 @@ const webpackHTMLConfig = new HtmlWebpackPlugin({
 const webpackModuleConfig = {
     rules: [
         {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+        {
             test: /\.m?js$/,
             exclude: /node_modules/,
             use: {
