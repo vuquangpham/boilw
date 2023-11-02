8dc0d739f42ac16d709f77df7220e8513310cb5a
vuquangpham <vuquangpham2909@gmail.com>
2023-11-02 12:49:04 +0700

    feat(instance): add destroy method

--- src/instance.js
+++ src/instance.js
@@ -2,4 +2,12 @@ export default class Instance{
     constructor(options){
         this.options = options;
     }
+
+
+    /**
+     * Destroy the instance
+     * */
+    destroy(){
+        return Library.destroy(this);
+    }
 }
