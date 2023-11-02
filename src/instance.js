0ab1c9df950b22f2c141df68faabe200d24fbcd9
vuquangpham <vuquangpham2909@gmail.com>
2023-11-02 12:51:59 +0700

    feat(instance): add on method for Events Emitter

--- src/instance.js
+++ src/instance.js
@@ -1,6 +1,22 @@
+import EventEmitter from "./events";
+
 export default class Instance{
     constructor(options){
         this.options = options;
+
+        // events emitter
+        this.events = new EventEmitter();
+    }
+
+
+    /**
+     * Register callback
+     * @param name {String}
+     * @param callback {Function}
+     * @return {void}
+     * */
+    on(name, callback){
+        this.events.on(name, callback);
     }
 
 
